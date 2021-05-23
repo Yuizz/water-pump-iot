e618b3a7bf98e75d37c2c44964eee002025b1856
Julian Abelardo Gonzalez Rodriguez <julianglz270@gmail.com>
2021-05-23 13:14:44 -0500

    Verification for WiFi refactored, added delay on servo moves

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,9 @@
 #include <Servo.h>
 #include <ESP8266WiFi.h>
 
-#define PIN_SERVO D2
+#include "credentials.h"
 
-const char *UBIDOTS_TOKEN = "BBFF-dRIAoPolqh0cwVMi6ha1YJS0bFhgY5"; // Put here your Ubidots TOKEN
-const char *WIFI_SSID = "NFINITUM518A_2.4_plus";                  // Put here your Wi-Fi SSID
-const char *WIFI_PASS = "9YMRaeUrHH";                              // Put here your Wi-Fi password
+#define PIN_SERVO D2
 
 const int OFF = 0;
 const int AUTO = 45;
@@ -26,46 +24,43 @@ void setup(){
   delay(1000);
   myServo.detach();
 
+  Serial.print("Connecting");
   ubidots.wifiConnect(WIFI_SSID, WIFI_PASS);
   servo_state = 0;
   previous_servo_state = servo_state;
 }
 
 void loop(){
-  if (!ubidots.wifiConnected()){
-    myServo.attach(PIN_SERVO);
-    myServo.write(AUTO);
-    delay(1000);
-    myServo.detach();
-    Serial.println("Reconnecting");
-    ubidots.wifiConnect(WIFI_SSID, WIFI_PASS);
-    tries++;
-  }else{
     servo_state = ubidots.get("ejc-main-esp8266", "Servo");
-    Serial.println(servo_state);
+    Serial.print(servo_state);
 
     if (servo_state != ERROR_VALUE){
+      tries = 0;
       if (servo_state != previous_servo_state){
         myServo.attach(PIN_SERVO);
+        delay(10);
         if (servo_state == 0) myServo.write(AUTO);
         if (servo_state == 1) myServo.write(MANUAL);
         previous_servo_state = servo_state;
-        delay(1000);
+        delay(500);
         myServo.detach();
       }
     }else{
-      Serial.println("Reconnecting");
+      Serial.println("...ERROR_VALUE");
+      Serial.print("Reconnecting");
       ubidots.wifiConnect(WIFI_SSID, WIFI_PASS);
       tries++;
-      if (myServo.read() == MANUAL) myServo.write(AUTO);
-      delay(1000);
-      myServo.detach();
+      if (myServo.read() == MANUAL){
+        myServo.attach(PIN_SERVO);
+        myServo.write(AUTO);
+        delay(500);
+        myServo.detach();
+        }
     }
 
-    delay(1000);
-  }
-  if(tries>=2){
+  if(tries>=5){
     Serial.println("Restarting...");
     ESP.restart();
   }
+    delay(1000);
 }
